7b1ccd4398fa69c0bf76120e8f4da30881817ed1
Ruvinda Dhambarage <ruvinda.dhambarage@nagarro.com>
2026-07-05 07:48:48 -0400

    Start dev env in source code folder

--- start_env.sh
+++ start_env.sh
@@ -6,11 +6,12 @@ export HWGID=$(id -g)
 echo "run: sudo update-binfmts --enable"
 
 docker run --rm --privileged -it \
-           --user $HWUID:$HWGID \
-	   --tmpfs /dev/shm:rw,exec \
-	   -v /etc/group:/etc/group:ro \
-	   -v /etc/passwd:/etc/passwd:ro \
-	   -v /etc/shadow:/etc/shadow:ro \
-           -v $PWD/code:/opt/code \
-           --name hw101 hw101
+    --user $HWUID:$HWGID \
+	--tmpfs /dev/shm:rw,exec \
+	-v /etc/group:/etc/group:ro \
+	-v /etc/passwd:/etc/passwd:ro \
+	-v /etc/shadow:/etc/shadow:ro \
+	-v $PWD/code:/opt/code \
+	-w /opt/code \
+	--name hw101 hw101
 
